1931850e484b70fd93149b19bb889b425118cf0e
Paul Aldred-Bann <paul.aldred-bann@defra.gov.uk>
2024-04-03 10:07:02 +0100

    added controller, route and view for app amendments

--- src/server/applications/controller.js
+++ src/server/applications/controller.js
@@ -16,19 +16,45 @@ export const getApplication = {
       params: { prefix, year, sequenceNumber }
     } = request
 
-    const { payload } = await Wreck.get(
+    const { payload: applicationJson } = await Wreck.get(
       `${config.get('backendApiUrl')}/applications/${prefix}/${year}/${sequenceNumber}`
     )
-
-    const application = JSON.parse(payload).value
-    const heading = `Application ${prefix}/${year}/${sequenceNumber}`
+    const application = JSON.parse(applicationJson).value
 
     return h.view('applications/index', {
-      pageTitle: heading,
-      heading,
+      pageTitle: `Application ${prefix}/${year}/${sequenceNumber}`,
+      heading: `Application ${prefix}/${year}/${sequenceNumber}`,
       breadcrumbs,
       application,
       formDisabled: true
+    })
+  }
+}
+
+export const getApplicationAmendment = {
+  method: 'GET',
+  path: '/applications/{prefix}/{year}/{sequenceNumber}/amend',
+  handler: async (request, h) => {
+    const {
+      params: { prefix, year, sequenceNumber }
+    } = request
+
+    const { payload: applicationJson } = await Wreck.get(
+      `${config.get('backendApiUrl')}/applications/${prefix}/${year}/${sequenceNumber}`
+    )
+    const { value: application } = JSON.parse(applicationJson)
+
+    const { payload: amendmentJson } = await Wreck.get(
+      `${config.get('backendApiUrl')}/applications/${prefix}/${year}/${sequenceNumber}/amendment-request`
+    )
+    const { value: amendment } = JSON.parse(amendmentJson)
+
+    return h.view('applications/amend', {
+      pageTitle: `Application ${prefix}/${year}/${sequenceNumber}`,
+      heading: `Application ${prefix}/${year}/${sequenceNumber}`,
+      breadcrumbs,
+      application,
+      amendment
     })
   }
 }
